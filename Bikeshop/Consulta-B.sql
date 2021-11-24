2caec0a4cf01be19662916350c0dfec3ba4a78ce
gastonhidalgocampos <83823992+gastonhidalgocampos@users.noreply.github.com>
2021-11-24 12:50:27 -0300

    Update Consulta-B.sql
    agregado el orden por productos

--- Bikeshop/Consulta-B.sql
+++ Bikeshop/Consulta-B.sql
@@ -2,7 +2,7 @@ select
 products.product_id, products.product_name,products.model_year,products.list_price
 from
 public.products 
-where product_name like '%Ladies%';--falta hacer la busqueda de "Ladies"
-
+where product_name like '%Ladies%'--falta hacer la busqueda de "Ladies"
+order by products.list_price desc;
 
 
