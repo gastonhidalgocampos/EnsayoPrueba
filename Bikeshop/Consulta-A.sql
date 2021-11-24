76df1c33464348f279497760f3dfb43031070da4
gastonhidalgocampos <83823992+gastonhidalgocampos@users.noreply.github.com>
2021-11-24 12:49:36 -0300

    Update Consulta-A.sql
    cambio en el orden del filtro

--- Bikeshop/Consulta-A.sql
+++ Bikeshop/Consulta-A.sql
@@ -3,4 +3,4 @@ products.product_id,products.product_name, products.model_year, products.list_pr
 from
 
 public.products where model_year=2016
-order by products.product_name desc
+order by products.product_name asc
